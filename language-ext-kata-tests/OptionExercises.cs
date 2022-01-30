9e5b689eaad6ced89a7209e7b8d3a66d92044458
Tr00d <guillaume.faas@live.fr>
2022-01-30 07:49:41 +0100

    Finishing OptionExercises

--- language-ext-kata-tests/OptionExercises.cs
+++ language-ext-kata-tests/OptionExercises.cs
@@ -14,14 +14,13 @@ namespace language_ext.kata.tests
         {
             // Filter this list with only defined persons
             var persons = Seq(
-                    None,
-                    Some(new Person("John", "Doe")),
-                    Some(new Person("Mary", "Smith")),
-                    None);
-
-
-            Seq<Person> definedPersons = Seq<Person>();
-
+                None,
+                Some(new Person("John", "Doe")),
+                Some(new Person("Mary", "Smith")),
+                None);
+            Seq<Person> definedPersons = persons
+                .Filter(person => person.IsSome)
+                .Map(person => person.IfNone(() => throw new InvalidOperationException()));
             Assert.Equal(2, definedPersons.Count);
         }
 
@@ -31,9 +30,10 @@ namespace language_ext.kata.tests
             // Instantiate a None Option of string
             // map it to an Upper case function
             // then it must return the string "Ich bin empty" if empty
+            Func<Option<string>, string> upperCase = parameter =>
+                parameter.Match(value => value.ToUpperInvariant(), "Ich bin empty");
             var iamAnOption = Option<string>.None;
-            string optionValue = null;
-
+            string optionValue = upperCase(iamAnOption);
             Assert.True(iamAnOption.IsNone);
             Assert.Equal("Ich bin empty", optionValue);
         }
@@ -42,8 +42,9 @@ namespace language_ext.kata.tests
         public void FindKaradoc()
         {
             // Find Karadoc in the people List or returns Perceval
-            var foundPersonLastName = "found";
-
+            var foundPersonLastName = this.people
+                .Find(person => person.LastName.Equals("Karadok"))
+                .Match(person => person.LastName, "Perceval");
             Assert.Equal("Perceval", foundPersonLastName);
         }
 
@@ -53,8 +54,9 @@ namespace language_ext.kata.tests
             // Find a person matching firstName and lastName, throws an IllegalArgumentException if not found
             var firstName = "Rick";
             var lastName = "Sanchez";
-
-            Assert.Throws<ArgumentException>(() => { });
+            Assert.Throws<ArgumentException>(() =>
+                this.people.Find(person => person.Named($"{firstName} {lastName}"))
+                    .IfNone(() => throw new ArgumentException()));
         }
 
         [Fact]
@@ -64,9 +66,14 @@ namespace language_ext.kata.tests
             // For each half append the value to the resultBuilder (side effect)
             double start = 500d;
             StringBuilder resultBuilder = new StringBuilder();
-
-            Option<double> result = Option<double>.Some(0);
-
+            Option<double> result = this.Half(start)
+                .Do(value => resultBuilder.Append(value))
+                .Bind(this.Half)
+                .Do(value => resultBuilder.Append(value))
+                .Bind(this.Half)
+                .Do(value => resultBuilder.Append(value))
+                .Bind(this.Half)
+                .Do(value => resultBuilder.Append(value));
             Assert.Equal(result, None);
             Assert.Equal("250125", resultBuilder.ToString());
         }
